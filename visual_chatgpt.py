4ba7fc5f2b5360ef1ca78e2bc117473459f728c5
ultago <83053171+panggu15@users.noreply.github.com>
2023-04-21 01:25:05 +0900

    Update visual_chatgpt.py

--- visual_chatgpt.py
+++ visual_chatgpt.py
@@ -801,7 +801,7 @@ class ImageVariation:
         self.seed = random.randint(0, 65535)
         seed_everything(self.seed)
         inp = self.tform(image).to(self.device).unsqueeze(0)
-        out = pipe(inp, guidance_scale=3)
+        out = self.pipe(inp, guidance_scale=3)
         updated_image_path = get_new_image_name(inputs, func_name="variation")
         out["images"][0].save(updated_image_path)
         print(f"\nProcessed ImageVariation, Input Image: {inputs}, Output Variation: {updated_image_path}")
